a6c46593bc49c252df63fb52d7065964878588a7
mariavanesalopez <mvanelopezsueldo579@gmail.com>
2023-02-22 23:06:53 -0300

    primer cambio

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+
+import './App.css'
+import Footer from './Components/Footer/Footer'
+import Header from './Components/Header/Header'
+import Main from './Components/Main/Main'
+
+function App() {
+ 
+
+  return (
+    <div className="App">
+      <Header />
+      <Main />
+      <Footer />
+    </div>
+  )
+}
+
+export default App
---
+++ src/Components/Main/Main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Main = () => {
+  return (
+    <div><h2>Main</h2></div>
+  )
+}
+
+export default Main
